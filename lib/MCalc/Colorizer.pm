c6af2e5ada17ae1550ffc2be63c999eb5e224617
Marc Hauptmann <marc.hauptmann@stud.uni-hannover.de>
2012-12-29 17:33:32 +0100

    added highlighting for matching braces

--- lib/MCalc/Colorizer.pm
+++ lib/MCalc/Colorizer.pm
@@ -4,11 +4,11 @@ use Moose;
 use Term::ANSIColor;
 use MCalc::Language;
 
-has baceColors => (isa => "ArrayRef",
+has baceStyle => (isa => "ArrayRef",
                    is => "rw",
-                   reader => "getBraceColors",
-                   writer => "setBraceColors",
-                   default => sub { ["green", "yellow", "blue", "magenta", "cyan"] });
+                   reader => "getBraceStyles",
+                   writer => "setBraceStyles",
+                   default => sub { ["bold green", "bold yellow", "bold blue", "bold cyan"] });
 
 has functionStyle => (isa => "Str",
                       is => "rw",
@@ -34,37 +34,70 @@ has keywordStyle => (isa => "Str",
                      writer => "setKeywordStyle",
                      default => sub { "bold blue" });
 
+has braceHighlightStyles => (isa => "ArrayRef",
+                             is => "rw",
+                             reader => "getBraceHighlightStyles",
+                             writer => "setBraceHighlightStyles",
+                             default => sub { ["bold green on_bright_magenta",
+                                               "bold yellow on_bright_magenta",
+                                               "bold blue on_bright_magenta",
+                                               "bold cyan on_bright_magenta",] });
+
 has context => (isa => "MCalc::Context",
                 is => "rw",
                 reader => "getContext",
                 writer => "setContext");
 
 sub colorize {
-  my ($this, $expression) = @_;
+  my ($this, $expression, $cursor) = @_;
 
   my @tokens = split(/([\(\)\+\-\*\/,\^=])/, $expression);
 
   my $level = 0;
   my $output = "";
+  my $pos = 0;
+  my $highlightLevel = -1;
+
+  if (defined($cursor)) {
+    $highlightLevel = $this->_determineBraceLevel($expression, $cursor);
+  }
 
   foreach my $token (@tokens) {
+    # handle opening brace
     if ($token eq "(" && $level >= 0) {
-      my $c = $this->getBraceColors()->[$level];
+      my $c;
+
+      if ($level == $highlightLevel) {
+        $c = $this->getBraceHighlightStyle($level);
+      } else {
+        $c = $this->getBraceStyle($level);
+      }
+
       $level++;
 
-      $output .= colored $token, $c." bold";
-    } elsif ($token eq ")" && $level >= 0) {
-      $level--;
-      my $c = $this->getBraceColors()->[$level];
+      $output .= colored($token, $c);
+    }
+    # handle closing brace
+    elsif ($token eq ")" && $level >= 0) {
+      my $c;
 
-      $output .= colored $token, $c." bold";
+      $level--;
+
+      if ($level == $highlightLevel) {
+        $c = $this->getBraceHighlightStyle($level);
+      } else {
+        $c = $this->getBraceStyle($level);
+      }
+
+
+      $output .= colored($token, $c);
     } elsif ($token =~ /[a-zA-Z]+/) {
       if ($this->getContext->functionIsDefined($token)) {
         $output .= colored($token, $this->getFunctionStyle());
       } elsif ($this->getContext->variableIsDefined($token)) {
         $output .= colored($token, $this->getVariableStyle());
       } elsif (is_keyword($token)) {
-	$output .= colored($token, $this->getKeywordStyle());
+        $output .= colored($token, $this->getKeywordStyle());
       } else {
         $output .= colored($token, $this->getUnknownStyle());
       }
@@ -74,6 +107,53 @@ sub colorize {
   }
 
   return $output;
+}
+
+sub getBraceHighlightStyle {
+  my ($this, $level) = @_;
+
+  my @styles = @{$this->getBraceHighlightStyles()};
+  my $numStyles = scalar(@styles);
+
+  return $styles[$level % $numStyles];
+}
+
+sub getBraceStyle {
+  my ($this, $level) = @_;
+
+  my @styles = @{$this->getBraceStyles};
+  my $numStyles = scalar(@styles);
+
+  return $styles[$level % $numStyles];
+}
+
+sub _determineBraceLevel {
+  my ($this, $string, $cursor) = @_;
+
+  my @tokens = split(/([\(\)])/, $string);
+
+  my $pos = 0;
+  my $level = -1;
+
+  foreach my $token (@tokens) {
+    if ($token eq "(") {
+      $level++;
+
+      if ($pos == $cursor) {
+        return $level;
+      }
+    } elsif ($token eq ")") {
+      if ($pos == $cursor - 1) {
+        return $level;
+      }
+
+      $level--;
+    }
+
+    $pos += length($token);
+  }
+
+  return -1;
 }
 
 1;
--- t/MCalc/Colorizer.t
+++ t/MCalc/Colorizer.t
@@ -12,11 +12,20 @@ BEGIN {
 
 sub braces_are_colored_bold : Tests {
   my $colorizer = MCalc::Colorizer->new();
-  $colorizer->setBraceColors(["green", "blue"]);
+  $colorizer->setBraceStyles(["bold green", "bold blue"]);
 
-  my $expected = colored("(", "green bold").colored(")", "green bold");
+  my $expected = colored("(", "bold green").colored(")", "bold green");
 
   is($colorizer->colorize("()"), $expected, "braces are colorized");
+}
+
+sub brace_colors_are_repeated : Tests {
+  my $colorizer = MCalc::Colorizer->new();
+  $colorizer->setBraceStyles(["green", "blue"]);
+
+  my $expected = colored("(", "green").colored("(", "blue").colored("(", "green");
+
+  is($colorizer->colorize("((("), $expected, "braces are colorized");
 }
 
 sub functions_are_underlined_by_default : Tests {
@@ -24,10 +33,10 @@ sub functions_are_underlined_by_default : Tests {
   $context->setFunction("sin", MCalc::Functions::UserFunction->new());
 
   my $colorizer = MCalc::Colorizer->new();
-  $colorizer->setBraceColors(["green", "blue"]);
+  $colorizer->setBraceStyles(["bold green", "bold blue"]);
   $colorizer->setContext($context);
 
-  my $expected = colored("sin", "underline bold").colored("(", "green bold")."1".colored(")", "green bold");
+  my $expected = colored("sin", "underline bold").colored("(", "bold green")."1".colored(")", "bold green");
 
   is($colorizer->colorize("sin(1)"), $expected, "function name is underlined");
 }
@@ -35,11 +44,11 @@ sub functions_are_underlined_by_default : Tests {
 sub unknown_identifiers_can_be_colored_red : Tests {
   my $context = MCalc::SimpleContext->new();
   my $colorizer = MCalc::Colorizer->new();
-  $colorizer->setBraceColors(["green", "blue"]);
+  $colorizer->setBraceStyles(["bold green", "bold blue"]);
   $colorizer->setUnknownStyle("red");
   $colorizer->setContext($context);
 
-  my $expected = colored("sin", "red").colored("(", "green bold")."1".colored(")", "green bold");
+  my $expected = colored("sin", "red").colored("(", "bold green")."1".colored(")", "bold green");
 
   is($colorizer->colorize("sin(1)"), $expected, "unknown function name is red");
 }
@@ -66,6 +75,30 @@ sub keyword_can_be_colored : Tests {
   my $expected = colored("quit", "blue bold");
 
   is($colorizer->colorize("quit"), $expected, "keyword is bold blue");
+}
+
+sub braces_are_highlighted : Tests {
+  my $context = MCalc::SimpleContext->new();
+
+  my $colorizer = MCalc::Colorizer->new();
+  $colorizer->setContext($context);
+  $colorizer->setBraceHighlightStyles(["red"]);
+  $colorizer->setUnknownStyle("");
+
+  my $expected = colored("sin", "").colored("(", "red")."3".colored(")", "red");
+
+  is($colorizer->colorize("sin(3)", 3), $expected);
+  is($colorizer->colorize("sin(3)", 6), $expected);
+}
+
+################################################################################
+
+sub brace_level_can_be_determined : Tests {
+  my $colorizer = MCalc::Colorizer->new();
+
+  is($colorizer->_determineBraceLevel("sin(x)", 3), 0, "opening brace increases level");
+  is($colorizer->_determineBraceLevel("sin(x)", 6), 0, "closing brace decreases level");
+  is($colorizer->_determineBraceLevel("sin(x)", 1), -1, "level -1 by default");
 }
 
 Test::Class->runtests;
